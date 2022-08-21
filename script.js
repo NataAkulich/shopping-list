91b0cad504c7af862ec3823c23e063e51b4ada99
NataAkulich <110620687+NataAkulich@users.noreply.github.com>
2022-08-21 15:05:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,49 @@
 /* Новые элементы должны добавляться в список по нажатию на Enter */
+const sendInput = document.querySelector('#input');
+const sendItems = document.querySelector('#items');
+
+
+
+sendInput.addEventListener('keydown', function (event) {
+    if (event.key == 'Enter') {
+
+        const messageText = sendInput.value;
+
+        const newMessage = document.createElement('li');
+        newMessage.classList.add('message');
+        newMessage.textContent = messageText;
+
+        if (messageText != '') {
+            sendItems.append(newMessage);
+        }
+
+        //for (let item of newMessage ) {
+
+        newMessage.addEventListener('click', function () {
+            newMessage.classList.toggle('done');
+
+        });
+
+
+        sendInput.value = '';
+
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /* Пустые элементы не должны добавляться */
 
